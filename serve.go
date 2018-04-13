ce802c9b616a078dcaff7c25a81726218a5a6545
Nate Meyers <natejmeyers@gmail.com>
2018-04-13 15:24:53 -0600

    inital commit of working program to serve index.html from any working directory by use of serve command

---
+++ serve.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+)
+
+func main() {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var ip net.IP
+	host, _ := os.Hostname()
+	addrs, _ := net.LookupIP(host)
+	for _, addr := range addrs {
+		if ipv4 := addr.To4(); ipv4 != nil {
+			ip = ipv4
+		}
+	}
+
+	fmt.Printf("Serving current directory %v on %v:8080", dir, ip)
+	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir(dir))))
+
+}
